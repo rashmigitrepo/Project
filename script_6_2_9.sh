8909487f0e58f65675cc7bf2be3d489c0accdbcc
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-08 09:54:15 -0500

    Create script_6_2_9.sh

---
+++ script_6_2_9.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+awk -F: '($1!~/(halt|sync|shutdown|nfsnobody|xman)/ && $7!~/^(\/usr)?\/sbin\/nologin(\/)?$/ && $7!~/(\/usr)?\/bin\/false(\/)?$/) { print $1 " " $6 }' /etc/passwd | while read -r user dir; do 
+  if [ ! -d "$dir" ]; then 
+    mkdir "$dir" 
+    chmod g-w,o-wrx "$dir" 
+    chown "$user" "$dir" 
+  fi 
+done
