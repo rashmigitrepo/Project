068b40347dfdb52071f9e9e923acd6069401a202
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-09 02:49:49 -0500

    Update script_6_2_11.sh

--- script_6_2_11.sh
+++ script_6_2_11.sh
@@ -4,6 +4,7 @@ awk -F: '($1!~/(halt|sync|shutdown|nfsnobody|xman)/ && $7!~/^(\/usr)?\/sbin\/nol
   if [ -d "$dir" ]; then 
     dirperm=$(stat -L -c "%A" "$dir") 
     if [ "$(echo "$dirperm" | cut -c6)" != "-" ] || [ "$(echo "$dirperm" | cut -c8)" != "-" ] || [ "$(echo "$dirperm" | cut -c9)" != "-" ] || [ "$(echo "$dirperm" | cut -c10)" != "-" ]; then 
+      echo "Default Permission is: \"$dirperm\" for your home directory: \"$dir\". But as per policy setting permission to \"drwxr-x---\" which is \"750\" in numerical form."      
       chmod g-w,o-rwx "$dir" 
     fi 
   fi 
