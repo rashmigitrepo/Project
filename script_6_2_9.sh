508a4d6a3298e35e5151aace4b3a1ca0def9d4ae
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-09 12:19:05 +0530

    Update script_6_2_9.sh

--- script_6_2_9.sh
+++ script_6_2_9.sh
@@ -2,7 +2,7 @@
 awk -F: '($1!~/(halt|sync|shutdown|nfsnobody|xman)/ && $7!~/^(\/usr)?\/sbin\/nologin(\/)?$/ && $7!~/(\/usr)?\/bin\/false(\/)?$/) { print $1 " " $6 }' /etc/passwd | while read -r user dir; do 
   if [ ! -d "$dir" ]; then 
     echo "User: \"$user\" home directory: \"$dir\" does not exist, creating home directory"
-     mkdir "$dir" 
+    mkdir "$dir" 
     chmod g-w,o-wrx "$dir" 
     chown "$user" "$dir" 
   fi 
