47c5e88f152e16245c5dda33b013a7a863c150a1
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-14 11:16:26 +0530

    Update script_5_5_1.sh

--- script_5_5_1.sh
+++ script_5_5_1.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash 
 for fn in system-auth password-auth; do 
   file="/etc/authselect/$(head -1 /etc/authselect/authselect.conf | grep 'custom/')/$fn"
-    if ! grep -Pq -- '^\h*password\h+requisite\h+pam_pwquality.so(\h+[^#\n\r]+)?\h+.*enforce_for_r oot\b.*$' "$file"; then 
+    if ! grep -Pq -- '^\h*password\h+requisite\h+pam_pwquality.so(\h+[^#\n\r]+)?\h+.*enforce_for_root\b.*$' "$file"; then 
       sed -ri 's/^\s*(password\s+requisite\s+pam_pwquality.so\s+)(.*)$/\1\2 enforce_for_root/' "$file" 
     fi 
     if grep -Pq -- '^\h*password\h+requisite\h+pam_pwquality.so(\h+[^#\n\r]+)?\h+retry=([4- 9]|[1-9][0-9]+)\b.*$' "$file"; then 
