9c772a559b2792ec15af501213c2d5e96ca37478
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-30 16:37:23 +0530

    Create script_6_2_13.sh

---
+++ script_6_2_13.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+awk -F: '($1!~/(halt|sync|shutdown|nfsnobody|xman)/ && $7!~/^(\/usr)?\/sbin\/nologin(\/)?$/ && $7!~/(\/usr)?\/bin\/false(\/)?$/) { print $6 }' /etc/passwd | while read -r dir; do 
+  if [ -d "$dir" ]; then 
+    file="$dir/.netrc" 
+    if [ ! -h "$file" ] && [ -f "$file" ]; then 
+      rm -f "$file" 
+    fi 
+  fi
+done
