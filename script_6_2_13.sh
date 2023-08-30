40df6a4d1afa00e9deef8bf1e42df19e61a5875a
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-30 16:37:55 +0530

    Update script_6_2_13.sh

--- script_6_2_13.sh
+++ script_6_2_13.sh
@@ -1,3 +1,4 @@
+#6.2.13
 #!/bin/bash 
 awk -F: '($1!~/(halt|sync|shutdown|nfsnobody|xman)/ && $7!~/^(\/usr)?\/sbin\/nologin(\/)?$/ && $7!~/(\/usr)?\/bin\/false(\/)?$/) { print $6 }' /etc/passwd | while read -r dir; do 
   if [ -d "$dir" ]; then 
